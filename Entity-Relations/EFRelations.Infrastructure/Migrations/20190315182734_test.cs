6d169874880ec7bde8f2889c7075b453d4b6e384
PlVasilev <pvvasilev2013@gmail.com>
2021-03-19 18:18:31 +0200

    Added EF Relation Project

---
+++ Entity-Relations/EFRelations.Infrastructure/Migrations/20190315182734_test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFRelations.Infrastructure.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    RegisteredOn = table.Column<DateTime>(nullable: false),
+                    Birthday = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Birthday", "PhoneNumber", "RegisteredOn", "FirstName", "LastName" },
+                values: new object[] { 1, new DateTime(1989, 5, 24, 0, 0, 0, 0, DateTimeKind.Unspecified), "76876876868", new DateTime(2019, 3, 15, 20, 27, 33, 152, DateTimeKind.Local).AddTicks(1877), "Petar", "Ivanov" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Birthday", "PhoneNumber", "RegisteredOn", "FirstName", "LastName" },
+                values: new object[] { 2, new DateTime(1989, 3, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "76876876868", new DateTime(2019, 3, 15, 20, 27, 33, 157, DateTimeKind.Local).AddTicks(3387), "Ivan", "Ivanov" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "Birthday", "PhoneNumber", "RegisteredOn", "FirstName", "LastName" },
+                values: new object[] { 3, new DateTime(1989, 9, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "76876876868", new DateTime(2019, 3, 15, 20, 27, 33, 157, DateTimeKind.Local).AddTicks(3410), "Ivan", "Petrov" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Entity-Relations/EFRelations.Infrastructure/Migrations/20190315185849_test1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFRelations.Infrastructure.Migrations
+{
+    public partial class test1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 58, 48, 532, DateTimeKind.Local).AddTicks(7839));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 58, 48, 537, DateTimeKind.Local).AddTicks(842));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 58, 48, 537, DateTimeKind.Local).AddTicks(881));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 27, 33, 152, DateTimeKind.Local).AddTicks(1877));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 27, 33, 157, DateTimeKind.Local).AddTicks(3387));
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3,
+                column: "RegisteredOn",
+                value: new DateTime(2019, 3, 15, 20, 27, 33, 157, DateTimeKind.Local).AddTicks(3410));
+        }
+    }
+}
